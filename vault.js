05c7a72912bab0b42e455523e347de22c092ca42
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2018-07-07 15:46:10 -1000

    Vault

--- vault.js
+++ vault.js
@@ -1,4 +1,22 @@
 'use strict';
 module.exports = function() {
-  
+  let storedKey = null;
+  let storedVal; 
+  return {
+      setValue: function(key, value) {
+        if (!(key)) {
+            return null;
+        }  else {
+            storedKey = key;
+            storedVal = value;
+        }
+      },
+      getValue: function(key) {
+          if ((!storedKey)) {
+              return null;
+          } else {
+              return storedVal;
+          }
+      }
+  }
 };
